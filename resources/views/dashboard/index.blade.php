630765dc85fa9cfad4c09306f5dcbfde3596a16e
dharnell <dharnell.a007@gmail.com>
2021-09-16 15:25:39 +0800

    Fixing dashboard

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <h1>WElcome to dashboard</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', 'App\Http\Controllers\DashboardController@index')->name('dashboard');
+});
