785ed2425dbb6455a2342b2bccec4bcd4f6918d6
jaycool75 <89345033+jaycool75@users.noreply.github.com>
2021-10-27 18:40:16 +0800

    Layouts

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<head>
+    @include('inc.main-head')
+</head>
+<body class="biolife-body">
+
+    @include('inc.main-header')
+    @yield('content')
+    @include('inc.main-footer')
+   
+</body>
+</html>
